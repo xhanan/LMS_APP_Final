e055be12bfd1c5cedc3499a8902548d569d0d607
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2021-10-12 21:35:12 +0200

    Fragments animations

--- app/src/main/java/com/example/lms_app_final/MainActivity.kt
+++ app/src/main/java/com/example/lms_app_final/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.lms_app
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.lms_app.data.entities.User
 import com.example.lms_app.data.entities.UserRole
 import com.example.lms_app.data.users.UserViewModel
 import com.example.lms_app.fragments.courses.AddCourseFragment
@@ -13,14 +11,10 @@ import com.example.lms_app.fragments.courses.CoursesFragment
 import com.example.lms_app.fragments.home.HomeFragment
 import com.example.lms_app.fragments.profile.ProfileFragment
 import com.example.lms_app_final.R
-import com.example.lms_app_final.fragments.lectures.AddLectureFragment
-import com.google.android.material.navigation.NavigationBarItemView
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_register.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -67,6 +61,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setCurrentFragment(fragment:Fragment)=
         supportFragmentManager.beginTransaction().apply {
+            setCustomAnimations(R.anim.enter_right_to_left,R.anim.exit_riht_to_left,R.anim.enter_left_to_right,R.anim.exit_left_to_right)
             replace(R.id.fragment_container,fragment)
             commit()
         }
